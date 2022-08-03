757d35a3721e4fe11bfb5b84a73af230c5d5d1b3
Leehan Ishrak <69171810+leehan561@users.noreply.github.com>
2022-08-03 12:04:26 +0000

    Login Page (HTML CSS)

---
+++ js/fireBase.js
@@ -0,0 +1,20 @@
+var app_firebase = {};
+
+(function(){
+  
+  var config = {
+apiKey: "AIzaSyDiI7HfD-vrqkyWR79X1f30sumidPsXnfo",
+    authDomain: "comp-menja-5f81f.firebaseapp.com",
+    databaseURL: "https://comp-menja-5f81f-default-rtdb.firebaseio.com",
+    projectId: "comp-menja-5f81f",
+    storageBucket: "comp-menja-5f81f.appspot.com",
+    messagingSenderId: "319617337601",
+    appId: "1:319617337601:web:7dc67ecd091089282c7300",
+    measurementId: "G-5FMTED301Q"
+  };
+
+  firebase.initializeApp(config);
+  app_firebase = firebase;
+
+})();
+
---
+++ js/login.js
@@ -0,0 +1,30 @@
+(function(){
+  // Initialize the FirebaseUI Widget using Firebase.
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        return true;
+      },
+
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'game.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
+
+})();
