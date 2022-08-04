594cc70828fc503b28dd418d1f48cf054764452d
Leehan Ishrak <69171810+leehan561@users.noreply.github.com>
2022-08-04 00:21:05 +0000

    Game Page (Working Game)

--- js/fireBase.js
+++ js/fireBase.js
@@ -3,14 +3,14 @@ var app_firebase = {};
 (function(){
   
   var config = {
-apiKey: "AIzaSyDiI7HfD-vrqkyWR79X1f30sumidPsXnfo",
-    authDomain: "comp-menja-5f81f.firebaseapp.com",
-    databaseURL: "https://comp-menja-5f81f-default-rtdb.firebaseio.com",
-    projectId: "comp-menja-5f81f",
-    storageBucket: "comp-menja-5f81f.appspot.com",
-    messagingSenderId: "319617337601",
-    appId: "1:319617337601:web:7dc67ecd091089282c7300",
-    measurementId: "G-5FMTED301Q"
+apiKey: "AIzaSyCbkpDdjPBOrkywwPQbmuZIQCTMRDF4CWs",
+    authDomain: "compfirebaseproject.firebaseapp.com",
+    databaseURL: "https://compfirebaseproject-default-rtdb.firebaseio.com",
+    projectId: "compfirebaseproject",
+    storageBucket: "compfirebaseproject.appspot.com",
+    messagingSenderId: "151892977917",
+    appId: "1:151892977917:web:188921344ba1cffd46f694",
+    measurementId: "G-JGHJYJEW6X"
   };
 
   firebase.initializeApp(config);
